109ba3eaef41681089540bc5b8dab9b9a5d46e69
UMR_Work <97790149+UMRwork@users.noreply.github.com>
2022-01-16 13:17:11 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,2 +1,18 @@
-
- 
+function myFunction1() {
+    location.replace("student-login.html")
+  }
+function myFunction2() {
+    location.replace("seller-login.html")
+  }
+function tocafev2() {
+    location.replace("cafe-v2.html")
+  }
+function tocafev3() {
+    location.replace("cafe-v3.html")
+  }
+function tocafev5() {
+    location.replace("cafe-v5.html")
+  }
+function tocafev6() {
+    location.replace("cafe-v6.html")
+  }
