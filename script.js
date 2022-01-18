a5a18b077996b3a1c8ee4b25003b631702575b7c
UMR_Work <97790149+UMRwork@users.noreply.github.com>
2022-01-19 00:40:30 +0800

    Update script.js

--- script.js
+++ script.js
@@ -4,6 +4,10 @@ function myFunction1() {
 function myFunction2() {
     location.replace("seller-login.html")
   }
+
+
+
+
 function tocafev2() {
     location.replace("cafe-v2.html")
   }
@@ -16,3 +20,62 @@ function tocafev5() {
 function tocafev6() {
     location.replace("cafe-v6.html")
   }
+
+
+
+
+
+function v2order1() {
+  alert("v2-1");
+}
+function v2order2() {
+  alert("v2-2");
+}
+function v2order3() {
+  alert("v2-3");
+}
+function v2order4() {
+  alert("v2-4");
+}
+
+
+function v3order1() {
+  alert("v3-1");
+}
+function v3order2() {
+  alert("v3-2");
+}
+function v3order3() {
+  alert("v3-3");
+}
+function v3order4() {
+  alert("v3-4");
+}
+
+
+function v5order1() {
+  alert("v5-1");
+}
+function v5order2() {
+  alert("v5-2");
+}
+function v5order3() {
+  alert("v5-3");
+}
+function v5order4() {
+  alert("v5-4");
+}
+
+
+function v6order1() {
+  alert("v6-1");
+}
+function v6order2() {
+  alert("v6-2");
+}
+function v6order3() {
+  alert("v6-3");
+}
+function v6order4() {
+  alert("v6-4");
+}
